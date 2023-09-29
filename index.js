735b5a74fd559fc5f038e09c3a398508747b95ce
adavilalith <132076255+adavilalith@users.noreply.github.com>
2023-09-29 19:14:43 +0530

    Updating index.js with animations

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 function place(){
     if (game_state==false){
         alert("reset game");
-        return null;
+        return;
     }
     if (this.textContent!=""){
         alert("full");
-        return null;
+        return;
     }
-    else{
     this.textContent=curr_turn;
     let num=this.getAttribute("id")[4];
     board[Number(num)]=curr_turn;
     if(check_winner()!=""){
+        if(check_winner()=="xo"){
+            turn_text.textContent="Draw";
+            game_state=false;
+            return ;
+        }
         turn_text.textContent=curr_turn+" is winner!";
         game_state=false;
         return ;
-    } 
+    }
+    console.log(1)
     if(curr_turn=="o"){
         curr_turn="x";
     } 
@@ -24,16 +29,31 @@ function place(){
     }
     
     turn_text.textContent=curr_turn+"\'s turn";
-    }
+
 }
 function reset_func(){
-    cells.forEach(cell => cell.textContent="");
+    cells.forEach(cell =>{
+        cell.textContent="";
+        cell.style.color="rgba(255, 255, 255, 1 )"; 
+        cell.style.animation="none";
+    });
     for(let i=0;i<9;i++){
         board[i]="";
     }
     curr_turn="x";
     turn_text.textContent=curr_turn+"\'s turn";
     game_state=true;
+}
+function changing_board_colours(win_condition){
+    
+    for(let i=0;i<9;i++){
+        if ((i==win_condition[0])||(i==win_condition[1])||(i==win_condition[2])){
+            cells[i].style.animation= "animation-winning 0.5s steps(6) 3";
+        }
+        else{
+            cells[i].style.color="rgba(255, 255, 255, 0.5 )";
+        }
+    }
 }
 function check_winner(){
     const win_condition=[[0,1,2],
@@ -44,19 +64,28 @@ function check_winner(){
                          [2,5,8],
                          [0,4,8],
                          [2,4,6]];
+    var flag=true;
     for(let i=0;i<8;i++){
+        if (board[i]==""){
+            flag=false;
+        }
         if ((board[win_condition[i][0]]=="x")&&
            (board[win_condition[i][1]]=="x")&&
            (board[win_condition[i][2]]=="x")){
+            changing_board_colours(win_condition[i]);
             return "x";
         }
         if ((board[win_condition[i][0]]=="o")&&
             (board[win_condition[i][1]]=="o")&& 
             (board[win_condition[i][2]]=="o")){
+            changing_board_colours(win_condition[i]);
             return "o";
         }
     }
-    return ""
+    if(flag){
+        return "xo";
+    }
+    return "";
 }
 var game_state=true;
 const board=["","","","","","","","",""];
